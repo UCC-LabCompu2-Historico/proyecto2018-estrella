682adfee670c4220707ba86fed641634c4a7a84a
CarolinaMisa <1700250@ucc.edu.ar>
2018-06-13 11:25:12 -0300

    Formulario javascript

--- desplazamiento.js
+++ desplazamiento.js
@@ -1,5 +1,9 @@
-function () {
-    
+function validar() {
+    var x = document.forms["formulario"]["fechayear"].value;
+    if (x == "") {
+        alert("Debe ingresar anio");
+        return false;
+    }
 }
 
-context.translate(50, 0);
+
